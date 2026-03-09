9ddb9fd0ef1220c5fe2968f3dea433f831df579b
FalkovskiyAV <avf-91@yandex.ru>
2026-03-09 19:04:47 +0300

    Фикс ordering-service

--- laptop-store/ordering-service/src/main/java/store/laptop/ordering/OrderingServiceApplication.java
+++ laptop-store/ordering-service/src/main/java/store/laptop/ordering/OrderingServiceApplication.java
@@ -4,10 +4,12 @@ package store.laptop.ordering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class OrderingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderingServiceApplication.class, args);
